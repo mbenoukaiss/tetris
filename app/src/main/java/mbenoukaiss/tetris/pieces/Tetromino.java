71a6821734cc2d28cce04f410eaaa336da1e3c5e
mbenoukaiss <marwane.benou-kaiss@etu.u-bordeaux.fr>
2019-02-10 19:57:46 +0100

    Updated Tetromino class structure

--- app/src/main/java/mbenoukaiss/tetris/pieces/Tetromino.java
+++ app/src/main/java/mbenoukaiss/tetris/pieces/Tetromino.java
@@ -1,48 +1,106 @@
 package mbenoukaiss.tetris.pieces;
 
-import android.graphics.Rect;
+import android.graphics.Point;
+import android.util.Size;
 
 import mbenoukaiss.tetris.Movable;
 
 public class Tetromino implements Movable {
 
-    private Rect position;
+    public enum Type {
+        I(0xFF00FFFF, 4, 1),
+        O(0xFFFFFF00, 2, 2),
+        T(0xFF551A8B, 3, 2),
+        L(0xFFFFA500, 3, 2),
+        J(0xFF0000FF, 3, 2),
+        Z(0xFFFF0000, 3, 2),
+        S(0xFF00FF00, 3, 2);
+
+        private final int color;
+        private final int width;
+        private final int height;
+
+        Type(int color, int width, int height) {
+            this.color = color;
+            this.width = width;
+            this.height = height;
+        }
+
+        public int[][] getLayout() {
+            switch(this) {
+                case I:
+                    return new int[][]{
+                            {1, 1, 1, 1}
+                    };
+                case O:
+                    return new int[][]{
+                            {1, 1},
+                            {1, 1}
+                    };
+                case T:
+                    return new int[][]{
+                            {1, 1, 1},
+                            {0, 1, 0}
+                    };
+                case L:
+                    return new int[][]{
+                            {1, 1, 1},
+                            {1, 0, 0}
+                    };
+                case J:
+                    return new int[][]{
+                            {1, 1, 1},
+                            {0, 0, 1}
+                    };
+                case Z:
+                    return new int[][]{
+                            {1, 1, 0},
+                            {0, 1, 1}
+                    };
+                case S:
+                    return new int[][]{
+                            {0, 1, 1},
+                            {1, 1, 0}
+                    };
+            }
+
+            throw new RuntimeException("Tetromino type was added but is not handled");
+        }
+
+    }
+
+    private Type type;
+    private Point position;
+    private Size size;
     private int[][] matrix;
-    private int color;
 
-    public int getX() {
-        return position.right;
+    Tetromino(Type type, Point position) {
+        this.type = type;
+        this.position = position;
+        this.size = new Size(type.width, type.height);
+        this.matrix = type.getLayout();
     }
 
-    public int getY() {
-        return position.top;
+    public Point getPosition() {
+        return new Point(position);
     }
 
-    public int getWidth() {
-        return position.left - position.right;
+    public Size getSize() {
+        return size;
     }
 
-    public int getHeight() {
-        return position.bottom - position.top;
-    }
-
-    private void setWidth(int width) {
-        position.right = position.left + width;
-    }
-
-    private void setHeight(int height) {
-        position.bottom = position.top + height;
+    public int getColor() {
+        return type.color;
     }
 
     @Override
     public void rotate() {
-        setHeight(matrix[0].length);
-        setWidth(matrix.length);
+        size = new Size(size.getHeight(), size.getWidth());
 
-        int[][] newMatrix = new int[getWidth()][getHeight()];
-        for(int i = 0; i < getWidth(); ++i) {
-            for(int j = 0; j < getHeight(); ++j) {
-                newMatrix[j][getWidth()-1-i] = matrix[i][j];
+        int[][] newMatrix = new int[size.getWidth()][size.getHeight()];
+        for(int i = 0; i < size.getWidth(); ++i) {
+            for(int j = 0; j < size.getHeight(); ++j) {
+                newMatrix[j][size.getWidth()-1-i] = matrix[i][j];
             }
         }
 
@@ -51,19 +109,17 @@ public class Tetromino implements Movable {
 
     @Override
     public void left() {
-        --position.left;
-        --position.right;
+        --position.x;
     }
 
     @Override
     public void right() {
-        ++position.left;
-        ++position.right;
+        ++position.x;
     }
 
     @Override
     public void down() {
-        ++position.top;
+        ++position.y;
     }
-    
+
 }
