a25341f7a9973cf4d9c33d3b26769d5436f5615b
mbenoukaiss <marwane.benou-kaiss@etu.u-bordeaux.fr>
2019-02-11 17:19:20 +0100

    Added buttons to move/rotate tetrominoes

--- app/src/main/java/mbenoukaiss/tetris/MainActivity.java
+++ app/src/main/java/mbenoukaiss/tetris/MainActivity.java
@@ -2,8 +2,10 @@ package mbenoukaiss.tetris;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.GridView;
-import android.widget.Toast;
+
+import mbenoukaiss.tetris.pieces.Tetromino;
 
 public class MainActivity extends Activity {
 
@@ -16,14 +18,27 @@ public class MainActivity extends Activity {
 
         GridView gridview = findViewById(R.id.gridview);
         gridview.setAdapter(new Adapter(this, gridview, game));
-        gridview.setHorizontalSpacing(1);
-        gridview.setVerticalSpacing(1);
+        gridview.setHorizontalSpacing(0);
+        gridview.setVerticalSpacing(0);
         gridview.setNumColumns(10);
         gridview.setNumColumns(game.getGridSize().getWidth());
 
-        gridview.setOnItemClickListener((parent, v, position, id) -> {
-            Toast.makeText(this, String.valueOf(position), Toast.LENGTH_LONG).show();
-            gridview.invalidateViews();
+        Button left = findViewById(R.id.left);
+        left.setOnClickListener(v -> {
+            if(game.getFallingTetromino().getPosition().x > 0)
+                game.getFallingTetromino().left();
         });
+
+        Button rotate = findViewById(R.id.rotate);
+        rotate.setOnClickListener(v -> game.getFallingTetromino().rotate());
+
+        Button right = findViewById(R.id.right);
+        right.setOnClickListener(v -> {
+            Tetromino t = game.getFallingTetromino();
+
+            if(t.getPosition().x + t.getSize().getWidth() < game.getGridSize().getWidth())
+                game.getFallingTetromino().right();
+        });
+
     }
 }
