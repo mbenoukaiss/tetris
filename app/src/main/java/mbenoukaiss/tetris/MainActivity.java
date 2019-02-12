6efac3748c8a5153e665fec6aa28ab6bd2c1ebec
mbenoukaiss <marwane.benou-kaiss@etu.u-bordeaux.fr>
2019-02-12 17:48:06 +0100

    Updated main activity style

--- app/src/main/java/mbenoukaiss/tetris/MainActivity.java
+++ app/src/main/java/mbenoukaiss/tetris/MainActivity.java
@@ -19,5 +19,15 @@ public class MainActivity extends Activity {
             Intent intent = new Intent(MainActivity.this, TetrisActivity.class);
             startActivity(intent);
         });
+
+        Button highscores = findViewById(R.id.highscores);
+
+        Button quit = findViewById(R.id.quit);
+        quit.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        });
     }
 }
